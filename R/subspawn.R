1b0a17a3b6660018c40f5810acc43ee933370a26
Eric C. Anderson <eric.anderson@noaa.gov>
2015-05-06 13:41:51 -0700

    Filled in the P_IBD function.

--- R/subspawn.R
+++ R/subspawn.R
@@ -1,5 +1,16 @@
 
 
+# define values while developing
+if(FALSE) {
+  Nf <- 70
+  Nm <- 70
+  nf <- 5
+  y <- 0.5
+  d <- 1
+  r <- 0.5
+  s <- 3
+}
+
 #' computes the prob of identity by descent of a randomly chosen pair of gene copies after one generation
 #' 
 #' This function will compute the prob of IBD after one model.  See the vignette for more of
@@ -14,5 +25,19 @@
 #' @return Returns a double.  Actually, it will return a vector of doubles if you want to do it for multiple values.
 #' @export
 P_ibd <- function(Nf, Nm, nf, y, d, r, s) {
+  denom <- (nf * y * s) + Nf - (nf * y)
+  H <- (nf * y * s) / denom
+  W <- (Nf - nf * y) / denom
   
+  d1 <- Nf + nf * (1 - y)
+  d2 <- Nm + d * nf * r
+  
+  ret <- H^2 * (1 + d) / (8 * d * nf) +
+    (W^2 / 8) * (
+      (1 / Nf) * (Nf / d1) ^ 2     +
+        (1 / nf) * ((nf * (1 - y)) / d1) ^ 2     +
+        (1 / Nm) * (Nm / d2) ^ 2       +
+        (1 / (d * nf)) * ((d * nf * r) / d2) ^ 2
+    )
+  ret
 }
